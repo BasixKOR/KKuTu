e7b064e3599acf0685228ab83fb5a288f6c34d0a
Basix <jsrtyu1120@gmail.com>
2017-10-14 17:35:53 +0900

    clipboard blocked

--- server/lib/web/public/js/keyblock.js
+++ server/lib/web/public/js/keyblock.js
@@ -2,9 +2,21 @@ Mousetrap.bind(['F12', 'Ctrl+Shift+I', 'Ctrl+Shift+K', 'Ctrl+Z', 'Ctrl+Y', 'Ctrl
 Object.defineProperty(window, 'prepareBot', {
   writable: false
 })
+var talk = document.getElementById('Talk')
 /*
-var mo = new MutationObserver(record => {
-  //node.nodeValue//record.oldValue
+var originalValue = Object.assign({}, talk.value)
+Object.defineProperty(talk, "value", {
+  set: function (a) {
+    if(/[가-힣]+/.test(originalValue) && a.length !== originalValue.length && /[ㄱㄴㄷㄹㅁㅂㅅㅇㅈㅊㅋㅌㅍㅎa-zA-Z0-9]/.test(a.slice(-1))) {
+      // 로깅 필요!!
+      // 입력이 "가"에서 바로 "가나"로 변하는 케이스를 감지함.
+    }
+    originalValue = a
+  },
+  get: _ => originalValue
 })
-mo.observe(document.getElementById('Talk'))
+// 보내지지 않는 버그 존재함
 */
+talk.onpaste = function (event) {
+  event.preventDefault()
+}
